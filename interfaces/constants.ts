55010332d8922be5131241024f8ae0e73027da3d
Itay Oshri <itay.oshri@gmail.com>
2023-07-20 00:34:02 +0300

    FIX: Operator ref

--- interfaces/constants.ts
+++ interfaces/constants.ts
@@ -1,4 +1,5 @@
 export enum operator {
-  Metropoline = 0,
-  // Egged = ...
+  Egged = 3,
+  Metropoline = 15,
+  // TODO: full list from https://open-bus-stride-api.hasadna.org.il/gtfs_agencies/list
 }
