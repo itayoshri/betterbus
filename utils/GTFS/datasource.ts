35c35c22d5636f11f5ca94c21292fd5060ae8e45
Itay Oshri <itay.oshri@gmail.com>
2023-08-06 17:03:04 +0300

    FEATURE: Fetch function for gtfs

--- utils/GTFS/datasource.ts
+++ utils/GTFS/datasource.ts
@@ -1,13 +1,28 @@
 import axios from 'axios'
+import { IGtfsRideStopsReqParams } from '../../interfaces/gtfs'
+
+const BASE_URL =
+  'https://open-bus-stride-api.hasadna.org.il/gtfs_ride_stops/list'
 
 type FetchFor = 'stations' | string
 
-interface IFetchData {}
+export function BuildQuery(data: IGtfsRideStopsReqParams) {
+  let res = ''
+  for (const key in data) {
+    if (Object.hasOwnProperty.call(data, key)) {
+      res += `&${key}=${data[key]}`
+    }
+  }
+  return res
+}
 
-export function buildFetchUrl(fetchFor: FetchFor, data: IFetchData) {
+export function buildFetchUrl(
+  fetchFor: FetchFor,
+  data: IGtfsRideStopsReqParams
+) {
   switch (fetchFor) {
-    case 'stations':
-      return ''
+    case 'stops':
+      return `${BASE_URL}?get_count=false${BuildQuery(data)}`
     default:
       return ''
   }
@@ -15,8 +30,8 @@ export function buildFetchUrl(fetchFor: FetchFor, data: IFetchData) {
 
 export async function fetchDataSource<T extends {}>(
   fetchFor: FetchFor,
-  data: IFetchData
-) {
+  data: IGtfsRideStopsReqParams
+): Promise<T> {
   const url = buildFetchUrl(fetchFor, data)
   const res = await axios.get<T>(url)
   return res.data
