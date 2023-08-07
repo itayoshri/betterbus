d37bd240fa61a51da7dce9c0704df797c409667c
Itay Oshri <itay.oshri@gmail.com>
2023-08-08 01:00:30 +0300

    REFACTOR

---
+++ test-app/app/api/eta/route.ts
@@ -0,0 +1,16 @@
+import { NextResponse } from 'next/server'
+import { IGtfsRideStopsReqParams, IGtfsStop } from '../../../../interfaces/gtfs'
+import { fetchDataSource } from '../../../../utils/GTFS/datasource'
+import { Line } from '../../../../utils/index'
+import { ETA } from '../../../../utils/ETA'
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url)
+
+  const line = new Line(
+    Number(searchParams.get('line')),
+    Number(searchParams.get('operator'))
+  )
+  await line.addStops()
+  return NextResponse.json(await line.GetETA(0, 1))
+}
